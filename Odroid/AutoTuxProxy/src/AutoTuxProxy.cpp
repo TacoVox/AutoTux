6355878cfa474c56bc05a487dcbc50bf7550c327
TacoVox <jonas.kahler@icloud.com>
2016-04-04 23:40:44 +0200

    Last changes for the night. Code still not working. Fix tmrw

--- Odroid/AutoTuxProxy/src/AutoTuxProxy.cpp
+++ Odroid/AutoTuxProxy/src/AutoTuxProxy.cpp
@@ -2,17 +2,26 @@
 // Created by jonas on 4/1/16.
 //
 #include <iostream>
+#include <memory>
+
+#include <opendavinci/odcore/data/Container.h>
 
 #include "../include/packetio/PacketBroadcaster.h"
 #include "serial/USBConnector.h"
 
 using namespace std;
+using namespace odcore::data;
 
-int main() {
+int main(int argc, char **argv) {
     cout << "Testing Packet Broadcaster!" << endl;
     packetio::PacketBroadcaster packetBroadcaster(argc, argv);
     packetBroadcaster;
     packetBroadcaster.runModule();
+
+    //Create pointer
+    Container *container;
+    //Set Broadcaster pointer
+    //packetBroadcaster.setSensorBoardDataContainer(container);
 
     cout << "Testing USBConnector!" << endl;
     USBConnector serial_obj;
