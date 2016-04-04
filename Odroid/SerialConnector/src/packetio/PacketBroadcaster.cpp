aa04b6ef076a35495ce65dd7a3acc77c9cdbea49
TacoVox <jonas.kahler@icloud.com>
2016-04-04 15:36:52 +0200

    Updated to Conference

--- Odroid/SerialConnector/src/packetio/PacketBroadcaster.cpp
+++ Odroid/SerialConnector/src/packetio/PacketBroadcaster.cpp
@@ -3,39 +3,40 @@
 //
 
 #include "PacketBroadcaster.h"
-#include <stdint.h>
+
 #include <iostream>
-#include <memory>
-#include <opendavinci/odcore/io/udp/UDPSender.h>
-#include <opendavinci/odcore/io/udp/UDPFactory.h>
 
 using namespace std;
-using namespace odcore;
-using namespace odcore::io::udp;
+using namespace odcore::base::module;
+using namespace odcore::data;
 
-void packetio::UDPBroadcaster::sendtest(void) {
-    //Send 100 times some shit
-    int i;
-    for(i = 0; i < 100; i++) {
-        send();
-    }
+packetio::PacketBroadcaster::PacketBroadcaster(const int32_t &argc, char **argv) :
+        TimeTriggeredConferenceClientModule(argc, argv, "TimeTriggeredSender"),
+        sensorBoardDataContainer(NULL), interrupted(false) {}
+
+packetio::PacketBroadcaster::~PacketBroadcaster() {}
+
+void packetio::PacketBroadcaster::setUp() {
+    cout << "Started the broadcasting component..." << endl;
 }
 
-void packetio::UDPBroadcaster::send(void) {
-    const string RECEIVER = "127.0.0.1";
-    const uint32_t PORT = 1111;
-
-    // We are using OpenDaVINCI's std::shared_ptr to automatically
-    // release any acquired resources.
-    try {
-        std::shared_ptr<UDPSender> udpsender(UDPFactory::createUDPSender(RECEIVER, PORT));
-        cout << "Sending data...\n";
-        udpsender->send("Hello World\r\n");
-    }
-    catch(string &exception) {
-        cerr << "Data could not be sent: " << exception << endl;
-    }
+void packetio::PacketBroadcaster::tearDown() {
+    cout << "Stopped the broadcasting component..." << endl;
 }
 
+odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode packetio::PacketBroadcaster::body() {
+    while (getModuleStateAndWaitForRemainingTimeInTimeslice() ==
+           odcore::data::dmcp::ModuleStateMessage::RUNNING && !interrupted) {
+        if(sensorBoardDataContainer != NULL) {
+            cout << "Broadcasting values..." << endl;
+            getConference().send(*sensorBoardDataContainer);
+        }
+    }
+    return odcore::data::dmcp::ModuleExitCodeMessage::OKAY;
+}
 
+void packetio::PacketBroadcaster::setSensorBoardDataContainer(
+        odcore::data::Container &sensorBoardDataContainer) {
+    this->sensorBoardDataContainer = &sensorBoardDataContainer;
+}
 
--- Odroid/SerialConnector/src/packetio/PacketBroadcaster.h
+++ Odroid/SerialConnector/src/packetio/PacketBroadcaster.h
@@ -1,15 +1,24 @@
 //
 // Created by Jonas Kahler on 4/1/16.
 //
+#include <opendavinci/odcore/base/module/TimeTriggeredConferenceClientModule.h>
+#include <opendavinci/odcore/data/Container.h>
 
 #ifndef SERIALCONNECTOR_UDPBROADCASTER_H
 #define SERIALCONNECTOR_UDPBROADCASTER_H
 
 namespace packetio {
-    class UDPBroadcaster {
+    class PacketBroadcaster : public odcore::base::module::TimeTriggeredConferenceClientModule {
     public:
-        void sendtest(void);
-        void send(void);
+        PacketBroadcaster(const int32_t &argc, char **argv);
+        odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode body();
+        void setSensorBoardDataContainer(odcore::data::Container &);
+        virtual ~PacketBroadcaster();
+    private:
+        virtual void setUp();
+        virtual void tearDown();
+        bool interrupted;
+        odcore::data::Container *sensorBoardDataContainer;
     };
 }
 
