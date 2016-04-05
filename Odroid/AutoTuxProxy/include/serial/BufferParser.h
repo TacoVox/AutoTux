04671c06ac0aee130aad3fedf83248ead5e68498
vryashkov <ivo.vryashkov@gmail.com>
2016-04-05 16:17:11 +0200

    added parser for the packets received from the serial

--- Odroid/AutoTuxProxy/include/serial/BufferParser.h
+++ Odroid/AutoTuxProxy/include/serial/BufferParser.h
@@ -1,4 +1,13 @@
 #ifndef BUFFERPARSER_H
 #define BUFFERPARSER_H
 
+class BufferParser
+{
+public:
+    BufferParser();
+    ~BufferParser();
+    double decode_packet(unsigned char *p);
+    int encode_packet(void);
+};
+
 #endif // BUFFERPARSER_H
