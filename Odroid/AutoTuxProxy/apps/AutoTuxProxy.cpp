2b4bd6e6ac4cdd5b8a97dc94fbba900889ec9d19
TacoVox <jonas.kahler@icloud.com>
2016-04-05 16:52:01 +0200

    Das funktioniert endlich mal...

--- Odroid/AutoTuxProxy/apps/AutoTuxProxy.cpp
+++ Odroid/AutoTuxProxy/apps/AutoTuxProxy.cpp
@@ -2,7 +2,7 @@
 // Created by jonas on 4/1/16.
 //
 #include <iostream>
-#include <memory>
+#include <thread>
 
 #include <opendavinci/odcore/data/Container.h>
 
@@ -15,25 +15,27 @@ using namespace odcore::data;
 
 int main(int argc, char **argv) {
     cout << "Testing Packet Broadcaster!" << endl;
-    packetio::PacketBroadcaster packetBroadcaster(argc, argv);
-    packetBroadcaster;
-    packetBroadcaster.runModule();
 
-    cout << "Testing USBConnector!" << endl;
-    USBConnector serial_obj;
-    serial_obj.connect();
-    serial_obj.read();
+    std::shared_ptr<packetio::PacketBroadcaster> packetBroadcaster(new packetio::PacketBroadcaster(argc, argv));
+    std::thread pbthread(&packetio::PacketBroadcaster::runModule, packetBroadcaster);
+
+    //cout << "Testing USBConnector!" << endl;
+    //USBConnector serial_obj;
+    //serial_obj.connect();
+    //serial_obj.read();
 
     //Here we have to start reading and then wrapping the shit
     //Create factory
     containerfactory::SBDContainer sbdContainer;
     //Create pointer
-    Container *container;
+    shared_ptr<Container> container(new Container());
     *container = sbdContainer.genSBDContainer(95.2);
     //Set Broadcaster pointer
-    packetBroadcaster.setSensorBoardDataContainer(container);
+    packetBroadcaster->setSensorBoardDataContainer(container);
 
-    serial_obj.disconnect();
+    //serial_obj.disconnect();
+
+    pbthread.join();
 
     return 0;
 }
