ae042dcfa9a1d28895b8f76af339940b6c17e6e7
TacoVox <jonas.kahler@icloud.com>
2016-04-05 10:54:27 +0200

    Mockup

--- Odroid/AutoTuxProxy/src/AutoTuxProxy.cpp
+++ Odroid/AutoTuxProxy/src/AutoTuxProxy.cpp
@@ -18,15 +18,17 @@ int main(int argc, char **argv) {
     packetBroadcaster;
     packetBroadcaster.runModule();
 
+    cout << "Testing USBConnector!" << endl;
+    USBConnector serial_obj;
+    serial_obj.connect();
+    serial_obj.read();
+
+    //Here we have to start reading and then wrapping the shit
     //Create pointer
     Container *container;
     //Set Broadcaster pointer
     //packetBroadcaster.setSensorBoardDataContainer(container);
 
-    cout << "Testing USBConnector!" << endl;
-    USBConnector serial_obj;
-    serial_obj.connect();
-    serial_obj.read();
     serial_obj.disconnect();
 
     return 0;
