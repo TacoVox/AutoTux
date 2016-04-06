cde458ea484d5515d4cd821e50838ebd18f700a0
TacoVox <jonas.kahler@icloud.com>
2016-04-06 14:47:03 +0200

    fix

--- Odroid/AutoTuxProxy/src/containerfactory/SBDContainer.cpp
+++ Odroid/AutoTuxProxy/src/containerfactory/SBDContainer.cpp
@@ -27,7 +27,7 @@ Container containerfactory::SBDContainer::genSBDContainer(vector<unsigned char> 
 
     //Iterate over the vector and put the stuff in the map
     int i = 1;
-    for(vector::iterator it = values.begin(); it != values.end(); ++it) {
+    for(vector<unsigned char>::iterator it = values.begin(); it != values.end(); ++it) {
         sensordata[i] = (double)*it;
         i++;
     }
