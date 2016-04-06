280c672a9e4f2d35bd7ecf9de0eace6a549da1ac
salkin91 <niklas.lecomte@hotmail.com>
2016-04-06 21:56:40 +0200

    Made changes so that the car can recieve data from the simvehicle

--- Odroid/Overtaking/src/Overtake.cpp
+++ Odroid/Overtaking/src/Overtake.cpp
@@ -4,13 +4,25 @@
 
 #include <iostream>
 
+#include "opendavinci/odcore/data/Container.h"
+
+#include "automotivedata/generated/automotive/VehicleControl.h"
+#include "automotivedata/generated/automotive/miniature/SensorBoardData.h"
+
+#include "opendavinci/GeneratedHeaders_OpenDaVINCI.h"
+#include "automotivedata/GeneratedHeaders_AutomotiveData.h"
+
 #include "Overtake.h"
 
 namespace over{
     namespace take{
-        using namespace std;
 
+        using namespace std;
+        using namespace odcore::base;
         using namespace odcore::base::module;
+        using namespace odcore::data;
+        using namespace automotive;
+        using namespace automotive::miniature;
 
         using namespace over::take;
 
@@ -27,11 +39,31 @@ namespace over{
         }
 
         odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode Overtake::body() {
-            int i;
-            for (int i = 0; i < 10; i++) {
-                cout << "This is the body" << endl;
-            }
 
+            while (getModuleStateAndWaitForRemainingTimeInTimeslice() == odcore::data::dmcp::ModuleStateMessage::RUNNING) {
+
+                // 1. Get most recent vehicle data:
+                Container containerVehicleData = getKeyValueDataStore().get(automotive::VehicleData::ID());
+                VehicleData vd = containerVehicleData.getData<VehicleData> ();
+
+                // 2. Get most recent sensor board data describing virtual sensor data:
+                Container containerSensorBoardData = getKeyValueDataStore().get(automotive::miniature::SensorBoardData::ID());
+                SensorBoardData sbd = containerSensorBoardData.getData<SensorBoardData> ();
+
+                // Create vehicle control data.
+                VehicleControl vc;
+
+                cout << "This is the current speed: "  << vd.getSpeed() << endl;
+
+                cout << "This is the amount of sensors: " << sbd.getNumberOfSensors() << endl;
+
+                // Create container for finally sending the data.
+                //Container c(Container::VEHICLECONTROL, vc);
+                // Send container.
+                //getConference().send(c);
+                cout << "This is printed" << endl;
+
+            }
             return odcore::data::dmcp::ModuleExitCodeMessage::OKAY;
         }
     }
