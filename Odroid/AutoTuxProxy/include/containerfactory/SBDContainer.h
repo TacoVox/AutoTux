93ec52be425a41372b4091a013aad1130ba205b3
TacoVox <jonas.kahler@icloud.com>
2016-04-06 14:43:55 +0200

    Fixed the vec again

--- Odroid/AutoTuxProxy/include/containerfactory/SBDContainer.h
+++ Odroid/AutoTuxProxy/include/containerfactory/SBDContainer.h
@@ -11,7 +11,7 @@ namespace containerfactory {
     class SBDContainer {
     public:
         static SBDContainer* instance(void);
-        odcore::data::Container genSBDContainer(std::vector);
+        odcore::data::Container genSBDContainer(std::vector<auto>);
     private:
         SBDContainer();
         SBDContainer(const SBDContainer&);
