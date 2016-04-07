a821dbd68d5c3d3d08225c9a723b755a9fa33817
TacoVox <jonas.kahler@icloud.com>
2016-04-07 12:18:12 +0200

    Lets see...

--- Odroid/DecisionMaker/apps/DecisionMaker.cpp
+++ Odroid/DecisionMaker/apps/DecisionMaker.cpp
@@ -1,11 +1,24 @@
 //
 // Created by Jonas Kahler on 4/7/16.
 //
+#include "packetio/PacketBroadcaster.h"
+#include <opendavinci/odcore/data/Container.h>
 #include <iostream>
+#include <thread>
 
 using namespace std;
+using namespace packetio;
+using namespace odcore::data;
 
-int main(void) {
+int main(int argc, char **argv) {
     cout << "Started the AutoTux DecisionMaker..." << endl;
+    shared_ptr<PacketBroadcaster> packetBroadcaster(new PacketBroadcaster(argc, argv));
+    thread pbthread(&PacketBroadcaster::runModule, packetBroadcaster);
+
+    shared_ptr<Container> container(new Container());
+
+    packetBroadcaster->setControlDataContainer(container);
+
+    pbthread.join();
     return 0;
 }
