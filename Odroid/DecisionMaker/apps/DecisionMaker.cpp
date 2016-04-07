ad6dc497454c7b79078308a315682055448cc36d
TacoVox <jonas.kahler@icloud.com>
2016-04-07 11:06:53 +0200

    initial

---
+++ Odroid/DecisionMaker/apps/DecisionMaker.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Jonas Kahler on 4/7/16.
+//
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    cout << "Started the AutoTux DecisionMaker..." << endl;
+    return 0;
+}
---
+++ Odroid/DecisionMaker/src/packetio/PacketBroadcaster.cpp
@@ -0,0 +1,43 @@
+//
+// Created by Jonas Kahler on 4/1/16.
+//
+
+#include "../../include/packetio/PacketBroadcaster.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace odcore::base::module;
+using namespace odcore::data;
+
+packetio::PacketBroadcaster::PacketBroadcaster(const int32_t &argc, char **argv) :
+        TimeTriggeredConferenceClientModule(argc, argv, "TimeTriggeredSender"),
+        controlDataContainer(NULL), interrupted(false) {}
+
+packetio::PacketBroadcaster::~PacketBroadcaster() {}
+
+void packetio::PacketBroadcaster::setUp() {
+    cout << "Started the broadcasting component..." << endl;
+}
+
+void packetio::PacketBroadcaster::tearDown() {
+    cout << "Stopped the broadcasting component..." << endl;
+}
+
+odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode packetio::PacketBroadcaster::body() {
+    while (getModuleStateAndWaitForRemainingTimeInTimeslice() ==
+           odcore::data::dmcp::ModuleStateMessage::RUNNING && !interrupted) {
+        cout << "Checking for vals" << endl;
+        if(controlDataContainer != NULL) {
+            cout << "Broadcasting values..." << endl;
+            getConference().send(*controlDataContainer);
+        }
+    }
+    return odcore::data::dmcp::ModuleExitCodeMessage::OKAY;
+}
+
+void packetio::PacketBroadcaster::setSensorBoardDataContainer(
+        std::shared_ptr<odcore::data::Container> sensorBoardDataContainer) {
+    this->controlDataContainer = sensorBoardDataContainer;
+}
+
