cb0ffad9d9cb66ba37a8f1c9933073fa4335e188
vryashkov <ivo.vryashkov@gmail.com>
2016-04-07 11:33:06 +0200

    ok

--- Odroid/AutoTuxProxy/apps/AutoTuxProxy.cpp
+++ Odroid/AutoTuxProxy/apps/AutoTuxProxy.cpp
@@ -19,21 +19,12 @@ int main(int argc, char **argv) {
     std::shared_ptr<packetio::PacketBroadcaster> packetBroadcaster(new packetio::PacketBroadcaster(argc, argv));
     std::thread pbthread(&packetio::PacketBroadcaster::runModule, packetBroadcaster);
 
-    //cout << "Testing USBConnector!" << endl;
-    //USBConnector serial_obj;
-    //serial_obj.connect();
-    //serial_obj.read();
+    cout << "Testing USBConnector!" << endl;
+    usb_connector::USBConnector serial_obj;
+    serial_obj.connect();
+    serial_obj.read();
 
-    //Here we have to start reading and then wrapping the shit
-    //Create factory
-    containerfactory::SBDContainer sbdContainer;
-    //Create pointer
-    shared_ptr<Container> container(new Container());
-    *container = sbdContainer.genSBDContainer(95.2);
-    //Set Broadcaster pointer
-    packetBroadcaster->setSensorBoardDataContainer(container);
-
-    //serial_obj.disconnect();
+    serial_obj.disconnect();
 
     pbthread.join();
 
