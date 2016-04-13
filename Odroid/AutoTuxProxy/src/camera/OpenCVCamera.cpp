7dda323046b99d35f9057f483d00bf19217306af
mrmurb <max@ig4.se>
2016-04-13 13:59:07 +0200

    Sources changed for OpenCV2

--- Odroid/AutoTuxProxy/src/camera/OpenCVCamera.cpp
+++ Odroid/AutoTuxProxy/src/camera/OpenCVCamera.cpp
@@ -13,60 +13,47 @@ namespace proxy {
 
         OpenCVCamera::OpenCVCamera(const string &name, const uint32_t &id, const uint32_t &width, const uint32_t &height, const uint32_t &bpp) :
             Camera(name, id, width, height, bpp),
-            m_capture(NULL),
-            m_image(NULL) {
-                m_capture = cvCaptureFromCAM(id);
-                if(m_capture) {
-                    cvSetCaptureProperty(m_capture, CV_CAP_PROP_FRAME_WIDTH, width);
-                    cvSetCaptureProperty(m_capture, CV_CAP_PROP_FRAME_HEIGHT, height);
+            m_vc(),
+            m_mat() {
+                m_vc.open(id);
+                if(m_vc.isOpened()) {
+                    m_vc.set(CV_CAP_PROP_FRAME_HEIGHT, height);
+                    m_vc.set(CV_CAP_PROP_FRAME_WIDTH, width);
                 } else {
                     cerr << "CameraProxy: Could not open camera '" << name << "' with ID: " << id << endl;
                 }
         }
 
         OpenCVCamera::~OpenCVCamera() {
-            if(m_capture) {
-                cvReleaseCapture(&m_capture);
-                m_capture = NULL;
+            if(m_vc.isOpened()) {
+                m_vc.release();
             }
         }
 
         bool OpenCVCamera::isValid() const {
-            return (m_capture != NULL);
+            return m_vc.isOpened();
         }
 
         bool OpenCVCamera::captureFrame() {
             bool returnVal = false;
-            if(m_capture != NULL) {
-                if(cvGrabFrame(m_capture)) {
-                    // TODO Unnesseccary code? Never using camera with BPP 1.
-                    if(getBPP() == 1) {
-                        IplImage *tmpFrame = cvRetrieveFrame(m_capture);
-
-                        if(m_image == NULL) {
-                            m_image = cvCreateImage(cvGetSize(tmpFrame), IPL_DEPTH_8U, 1);
-                        }
-
-                        cvCvtColor(tmpFrame, m_image, CV_BGR2GRAY);
-                    } else {
-                        m_image = cvRetrieveFrame(m_capture);
-                    }
-                    returnVal = true;
+            if (m_vc.isOpened()) {
+                if (m_vc.grab()) {
+                    m_vc.retrieve(m_mat);
                 }
+                returnVal = true;
             }
             return returnVal;
         }
 
+
         bool OpenCVCamera::copyImageTo(char *dest, const uint32_t &size) {
             bool returnVal = false;
+            if((dest != NULL) && (size > 0)) {
+                memcpy(dest, m_mat.data, size);
 
-            if((dest != NULL) && (size > 0) && (m_image != NULL)) {
-                memcpy(dest, m_image->imageData, size);
-
-                // TODO Put in a debug if-statement.
-                cvShowImage("WindowShowImage", m_image);
-                cvWaitKey(10);
-                //
+                // TODO Read debug config.
+                imshow("WindowShowImage", m_mat);
+                waitKey(10);
                 returnVal = true;
             }
             return returnVal;
