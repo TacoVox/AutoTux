70c2781b9c0e6ca05b11be775a03f370fcc49447
TacoVox <jonas.kahler@icloud.com>
2016-04-14 11:13:14 +0200

    Forget that shit

--- Odroid/AutoTuxProxy/src/packetio/PacketBroadcaster.cpp
+++ Odroid/AutoTuxProxy/src/packetio/PacketBroadcaster.cpp
@@ -42,9 +42,5 @@ odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode packetio::PacketBroadc
 void packetio::PacketBroadcaster::setSensorBoardDataContainer(
         std::shared_ptr<odcore::data::Container> sensorBoardDataContainer) {
     cout << "Set SBDC..." << endl;
-    //If there is data set - remove it from memory
-    //if(this->sensorBoardDataContainer != NULL) {
-    //    this->sensorBoardDataContainer->~Container();
-    //}
     this->sensorBoardDataContainer = sensorBoardDataContainer;
 }
