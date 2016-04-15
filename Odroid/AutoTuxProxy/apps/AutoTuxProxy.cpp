6a5a0c525d4fa4a2d430ad92493382ac66f6a49b
vryashkov <ivo.vryashkov@gmail.com>
2016-04-15 15:48:53 +0200

    ok

--- Odroid/AutoTuxProxy/apps/AutoTuxProxy.cpp
+++ Odroid/AutoTuxProxy/apps/AutoTuxProxy.cpp
@@ -4,8 +4,6 @@
 #include <iostream>
 #include <thread>
 #include <signal.h>
-<<<<<<< HEAD
-#include "camera/CameraProxy.h"
 #include "serial/BufferWrapper.h"
 #include "proxy/Proxy.h"
 #include "serial/USBHandler.h"
@@ -14,12 +12,6 @@ using namespace std;
 using namespace serial;
 using namespace usb_handler;
 using namespace proxy::camera;
-=======
-#include <proxy/Proxy.h>
-
-
-using namespace std;
->>>>>>> e28127035b45501a400dd34412bf52772b0e19a1
 
 void exit_handler(int);
 
@@ -29,7 +21,6 @@ int32_t main(int32_t argc, char **argv) {
 
     cout << "Starting up AutoTuxProxy..." << endl;
 
-<<<<<<< HEAD
     shared_ptr<BufferWrapper> bw = (shared_ptr<BufferWrapper>)new BufferWrapper();
 
     proxy::Proxy proxy(argc, argv, bw);
@@ -43,11 +34,6 @@ int32_t main(int32_t argc, char **argv) {
     cout << "SerialHandler stopped" << endl;
 
     return 0;
-=======
-    proxy::Proxy p(argc, argv);
-
-    return p.runModule();
->>>>>>> e28127035b45501a400dd34412bf52772b0e19a1
 }
 
 
