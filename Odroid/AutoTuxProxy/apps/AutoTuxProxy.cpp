0d863a2aa5254fd54973fd44768722a5892844d2
TacoVox <jonas.kahler@icloud.com>
2016-04-18 14:19:55 +0200

    Fixed the main

--- Odroid/AutoTuxProxy/apps/AutoTuxProxy.cpp
+++ Odroid/AutoTuxProxy/apps/AutoTuxProxy.cpp
@@ -23,17 +23,20 @@ int32_t main(int32_t argc, char **argv) {
 
     shared_ptr<BufferWrapper> bw = (shared_ptr<BufferWrapper>)new BufferWrapper();
 
-    shared_ptr<proxy::Proxy> prx = (shared_ptr<proxy::Proxy>) new proxy::Proxy(argc, argv, bw);
-    thread prxthread(&proxy::Proxy::runModule, prx);
+    //shared_ptr<proxy::Proxy> prx = (shared_ptr<proxy::Proxy>) new proxy::Proxy(argc, argv, bw);
+    //thread prxthread(&proxy::Proxy::runModule, prx);
 
     shared_ptr<USBHandler> uc = (shared_ptr<USBHandler>) new USBHandler(bw);
     thread ucthread(&USBHandler::run, uc);
 
+    proxy::Proxy proxy(argc, argv, bw);
+    proxy.runModule();
+
     //Waiting for the thread to terminate
     ucthread.join();
     cout << "USBHandler stopped" << endl;
-    prxthread.join();
-    cout << "Proxy stopped" << endl;
+    //prxthread.join();
+    //cout << "Proxy stopped" << endl;
 
     return 0;
 }
