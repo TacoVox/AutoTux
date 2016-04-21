58ad6271eb34bba442d4d0eab3e2eb5454fc913d
salkin91 <niklas.lecomte@hotmail.com>
2016-04-21 16:31:17 +0200

    blabla

--- Odroid/DecisionMaker/include/parker/Parker.h
+++ Odroid/DecisionMaker/include/parker/Parker.h
@@ -21,8 +21,8 @@ namespace parker {
         const double SPOT_SIZE = 0.70;
         const double ADJUST_BEFORE_PARKING = 0.20;
         const double BACK_AROUND_CORNER = 0.45;
-        const double BACKING_STRAIGHT = 0.23;
-        const double BACKING_LEFT = 0.32;
+        const double BACKING_STRAIGHT = 0.20;
+        const double BACKING_LEFT = 0.38;
         const double ADJUST_IN_SPOT_FORWARD = 0.10;
         const double ADJUST_IN_SPOT_BACK = 0.10;
 
