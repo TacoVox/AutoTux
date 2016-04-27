7ac3cb9c6bffe0c81003c6aafcc6ff273d939045
TacoVox <jonas.kahler@icloud.com>
2016-04-27 14:48:49 +0200

    this will be a window showing the current status of the session

--- Odroid/ATConfigurator/src/ui/ValMonitor.cpp
+++ Odroid/ATConfigurator/src/ui/ValMonitor.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "ui/ValMonitor.h"
+
+ui::ValMonitor::ValMonitor(void) : xsize(78), ysize(38),
+                                   _valmonitor(newwin(1, xsize, 1, 0)) {}
+
+ui::ValMonitor::ValMonitor(int x, int y) : xsize(x - 2), ysize(y - 2),
+                                       _valmonitor(newwin(1, xsize, 1, 0)) {
+}
+
