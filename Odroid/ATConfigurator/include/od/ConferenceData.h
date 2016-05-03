40a026cf2bff2490b94fe28df1cb4aea58ab2490
TacoVox <jonas.kahler@icloud.com>
2016-05-03 10:39:06 +0200

    Added necessary fields

--- Odroid/ATConfigurator/include/od/ConferenceData.h
+++ Odroid/ATConfigurator/include/od/ConferenceData.h
@@ -14,11 +14,29 @@ namespace od {
         static ConferenceData* instance(void);
         double getSpeed(void);
         void setSpeed(double);
+        double getAngle(void);
+        void setAngle(double);
+        double getUS1(void);
+        void setUS1(double);
+        double getUS2(void);
+        void setUS2(double);
+        double getIR1(void);
+        void setIR1(double);
+        double getIR2(void);
+        void setIR2(double);
+        double getIR3(void);
+        void setIR3(double);
     private:
         ConferenceData();
         ConferenceData(const ConferenceData&);
         ~ConferenceData();
         double speed;
+        double angle;
+        double us1;
+        double us2;
+        double ir1;
+        double ir2;
+        double ir3;
         static ConferenceData* _instance;
     };
 }
--- Odroid/ATConfigurator/src/od/ConferenceData.cpp
+++ Odroid/ATConfigurator/src/od/ConferenceData.cpp
@@ -20,15 +20,26 @@ double od::ConferenceData::getSpeed(void) { return this->speed; }
 
 void od::ConferenceData::setSpeed(double s) { this->speed = s; }
 
+double od::ConferenceData::getAngle(void) { return this->angle; }
 
+void od::ConferenceData::setAngle(double a) { this->angle = a; }
 
+double od::ConferenceData::getUS1(void) { return this->us1; }
 
+void od::ConferenceData::setUS1(double us) { this->us1 = us; }
 
+double od::ConferenceData::getUS2(void) { return this->us2; }
 
+void od::ConferenceData::setUS2(double us) { this->us2 = us; }
 
+double od::ConferenceData::getIR1(void) { return this->ir1; }
 
+void od::ConferenceData::setIR1(double ir) { this->ir1 = ir; }
 
+double od::ConferenceData::getIR2(void) { return this->ir2; }
 
+void od::ConferenceData::setIR2(double ir) { this->ir2 = ir; }
 
+double od::ConferenceData::getIR3(void) { return this->ir3; }
 
-
+void od::ConferenceData::setIR3(double ir) { this->ir3 = ir; }
