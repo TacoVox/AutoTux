921c2a766f0dc8dff8beea807356744532840a79
TacoVox <jonas.kahler@icloud.com>
2016-05-04 13:22:02 +0200

    Received now the recommended data

--- Odroid/ATConfigurator/src/od/ConferenceModule.cpp
+++ Odroid/ATConfigurator/src/od/ConferenceModule.cpp
@@ -8,12 +8,14 @@
 #include <automotivedata/generated/automotive/VehicleControl.h>
 #include <automotivedata/generated/automotive/miniature/SensorBoardData.h>
 #include <automotivedata/generated/autotux/config/LaneFollowerMSG.h>
+#include <automotivedata/generated/autotux/LaneRecommendationMSG.h>
 
 using namespace std;
 using namespace odcore::base::module;
 using namespace odcore::data;
 using namespace automotive;
 using namespace automotive::miniature;
+using namespace autotux;
 using namespace autotux::config;
 
 od::ConferenceModule::ConferenceModule(const int32_t &argc, char **argv) :
@@ -35,7 +37,7 @@ odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode od::ConferenceModule::
         ConferenceData::instance()->setSpeed(vehicleControl.getSpeed());
         ConferenceData::instance()->setAngle(vehicleControl.getSteeringWheelAngle());
 
-        //Get the current xyz
+        //Get the current sensor board data
         Container sbc = getKeyValueDataStore().get(SensorBoardData::ID());
         SensorBoardData sensorBoardData = sbc.getData<SensorBoardData>();
         map<uint32_t, double> sensorMap = sensorBoardData.getMapOfDistances();
@@ -44,6 +46,13 @@ odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode od::ConferenceModule::
         ConferenceData::instance()->setIR1(sensorMap[0]);
         ConferenceData::instance()->setIR2(sensorMap[2]);
         ConferenceData::instance()->setIR3(sensorMap[1]);
+
+        //Get the current LaneRecommendation
+        Container lrc = getKeyValueDataStore().get(LaneRecommendationMSG::ID());
+        LaneRecommendationMSG laneRecommendationMSG = lrc.getData<LaneRecommendationMSG>();
+        ConferenceData::instance()->setRec_angle(laneRecommendationMSG.getAngle());
+        ConferenceData::instance()->setDistance_to_line(laneRecommendationMSG.getDistance_to_line());
+        ConferenceData::instance()->setQuality(laneRecommendationMSG.getQuality());
 
         if(od::ConferenceData::instance()->isNewData()) {
             getConference().send(*od::ConferenceData::instance()->genLaneFollowerContainer());
