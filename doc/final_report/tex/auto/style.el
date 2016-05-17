657d6c4d88a47efad1f4dc7c50706db383e48e6b
TacoVox <jonas.kahler@icloud.com>
2016-05-17 15:13:20 +0200

    Update

---
+++ doc/final_report/tex/auto/style.el
@@ -0,0 +1,23 @@
+(TeX-add-style-hook
+ "style"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("xcolor" "table")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "eurosym"
+    "titlesec"
+    "hyperref"
+    "fancyhdr"
+    "graphicx"
+    "tabu"
+    "multirow"
+    "xcolor")))
+
