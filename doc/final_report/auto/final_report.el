1b7a8a54ca6138d77cf72b2cf12c66d0e5ebb17d
TacoVox <jonas.kahler@icloud.com>
2016-05-18 14:47:44 +0200

    Added the 9th chap

--- doc/final_report/auto/final_report.el
+++ doc/final_report/auto/final_report.el
@@ -13,7 +13,7 @@
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
-    "tex/ch7"
+    "tex/ch9"
     "report"
     "rep12"
     "tex/style")))
